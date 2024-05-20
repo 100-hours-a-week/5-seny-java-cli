39b844a42f2cdefebc85caa497e792e762c375d9
sen2y <pse6704@sookmyung.ac.kr>
2024-05-20 23:13:00 +0900

    CGV 매점 키오스크 start

---
+++ src/main/java/com/product/Product.java
@@ -0,0 +1,53 @@
+package com.product;
+
+public class Product {
+    private String name;
+    private int price;
+    private String size;
+    private int plusPrice = 0;
+
+    public Product(String name, int price, String size) {
+        this.name = name;
+        this.price = price;
+        this.size = size;
+    }
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+        this.size = "M";
+    }
+
+    public Product(String name, int price, String size, int plusPrice) {
+        this.name = name;
+        this.price = price;
+        this.size = size;
+        this.plusPrice = plusPrice;
+    }
+
+    public Product(String name, int price, int plusPrice) {
+        this.name = name;
+        this.price = price;
+        this.size = "M";
+        this.plusPrice = plusPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String printMenu() {
+        if (plusPrice > 0) {
+            return name + " " + price + "원 " + size + " (+" + plusPrice + "원)";
+        }
+        return name + " " + price + "원 " + size + " ";
+    }
+}
